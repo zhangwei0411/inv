50980f25526c56c662f3b2a20767775ba3271c3e
zhangwei0411 <719699749@qq.com>
2018-03-14 18:16:43 +0800

    test

--- bin/test_tushare.py
+++ bin/test_tushare.py
@@ -1,6 +1,21 @@
 import tushare as ts
 day = '0313'
-info = '../data/' + day + '_concept.csv'
-#print(ts.get_industry_classified())
-df = ts.get_concept_classified()
-df.to_csv(info,encoding="utf-8")
+concept = '../data/' + day + '_concept.csv'
+industry = '../data/' + day + '_industry.csv'
+index = '../data/' + day + '_index.csv'
+def get_industry():
+    df = ts.get_industry_classified()
+    df.to_csv(industry, encoding="utf-8")
+
+def get_concept():
+    df = ts.get_concept_classified()
+    df.to_csv(concept,encoding="utf-8")
+
+def get_index():
+    df = ts.get_index()
+    df.to_csv(index, encoding="utf-8")
+
+if __name__ == '__main__':
+    #get_concept()
+    #get_industry()
+    get_index()
